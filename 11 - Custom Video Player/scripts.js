81d4ded1cc81c3b9a17b386011ed54e934932da0
yv <yavictor37@yandex.ru>
2021-01-15 13:07:19 +0300

    Walk through 11 day challenge

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,54 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progressBar');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('.skipButtons');
+const ranges = player.querySelector('.playerr_slider');
+
+function togglePlay() {
+  if (video.pause) {
+    video.play()
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  //console.log(icon);
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
